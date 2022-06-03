e70992c46bcb33ee8924193b653a9f80879a5aad
carleii <devcarle@gmail.com>
2022-06-03 13:54:34 +0200

    increase total hour wasted

--- bot.py
+++ bot.py
@@ -1,3 +1,16 @@
+# Dear programmer:
+#When i wrote this code, only god and
+#I knew how it worked.
+#Now, only gog knows it !
+
+#Therefore, if you are trying to optimize
+#this routine and it fails (most surely),
+#please increase this counter as a 
+#warning for the next person:
+
+#total_hours_wasted_here = 28
+
+
 import tweepy
 import config
 
